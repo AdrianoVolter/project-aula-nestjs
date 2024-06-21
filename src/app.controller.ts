4823ee9d924361a6a43284e81a2c3a7b0a71f116
adrianovolter <adriano.j.volter@hotmail.com>
2024-06-21 10:23:19 -0300

    verbo post teste

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,8 +10,8 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get("/ola")
-  getOla(): string {
-    return "Ola dev!";
+  @Post("/ola") // http://localhost:3000/ola
+  getOla(): string {  // fun
+    return this.appService.getOla();
   }
 }
