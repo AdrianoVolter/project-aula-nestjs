b3d5c4ffcacb23718bbdf1b4d31b247081af12a1
adrianovolter <adriano.j.volter@hotmail.com>
2024-06-21 19:18:34 -0300

    validando patch

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,39 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 export type User = {
-    id?: number;
-    name: string;
-    age: number;
-    uf: string;
+  id?: number;
+  name: string;
+  age: number;
+  uf: string;
 };
-
 
 @Injectable()
 export class UserService {
+  nexId: number = 1;
+  users: User[] = [];
 
-    nexId: number = 1;
-    users: User[] = [];
+  getUsers(): User[] {
+    return this.users;
+  }
 
-    getUsers(): User[] {
-        return this.users 
+  addUser(newUser: User): User {
+    if (!newUser.id) {
+      newUser.id = this.nexId++;
     }
+    this.users.push(newUser);
+    return newUser;
+  }
 
-    addUser(newUser: User): User {
-        if (!newUser.id) {
-            newUser.id = this.nexId++;
-        }
-        this.users.push(newUser);
-        return newUser;
-    }
-
-    patchUser(id: number ,userData: User): User {
-        const user = this.users.find(user => user.id === id);
-        if (!user) {
-            return null;
-        }
-        Object.assign(user, userData);
-        return user;
-
-    }
-
+  patchUser(id: number, userData: User): User {
+    const index = this.users.findIndex(u => u.id == id);
+    if(index < 0) throw new NotFoundException();
+    const user = this.users[index];
+    if(userData.name)
+        user.name = userData.name;
+    if(userData.age)
+        user.age = userData.age;
+    if(userData.uf)
+        user.uf = userData.uf;
+    this.users[index] = user;
+    return user;
+  }
 }
