d7a54a19f5b2d2866eac4019a650de07f4f9679d
adrianovolter <adriano.j.volter@hotmail.com>
2024-06-21 10:36:40 -0300

    criado pasta e arquivos user, via cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user/user.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
